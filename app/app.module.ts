be939187c11231921bacb645d53b71617f08055c
ravira <ravira@ncs.com.sg>
2017-06-23 19:12:01 +0800

    First Component

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import { EventsAppComponent } from './events-app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [EventsAppComponent],
+    bootstrap: [EventsAppComponent]
+})
+export class AppModule {
+
+}
